50939f8d58871f1fbb624149ad6b532974cbd7ab
Werick <werick.vieira@yahoo.com.br>
2015-10-12 16:57:38 -0300

    teste

--- assets/main.js
+++ assets/main.js
@@ -1,29 +1,29 @@
 
+	
 	var app = angular.module("exemploService",[]);
-	    app.controller("meuControler", meuControler );
-	    app.service("meuService", meuService);
+	    app.controller("meuControler", minhaFuncaoControler );
+	    app.service("meuService", minhaFuncaoService);
 
-		    function meuControler ( $scope, meuService ){
-			  function sucesso ( obj ){
-		        $scope.User = obj.data;
-			  }
-			  function erro ( err ){
-			    console.log( err );
-			  }; 
-
-			  meuService.then(sucesso, erro);
-			  
+		    function minhaFuncaoControler ( $scope, callback ){
+				function sucesso ( obj ){
+			        $scope.User = obj.data;
+				  };
+				function erro ( err ){
+				    console.log( err );
+				  }; 
+				callback.then(sucesso, erro);
 	        }; // Controller 
 
-	    function meuService( $http ) {
-	         var Requisicao = {
-		        url:'https://api.github.com/users/werickvieira',
-		        method:'GET'
-		      };
-			 return $http(Requisicao);
-	    }; // Service 
+	        function minhaFuncaoService( $http ){
+		         var Requisicao = {
+			        url:'https://api.github.com/users/werickvieira',
+			        method:'GET'
+			      };
+				 return $http(Requisicao);
+	        }; // Service 
 
+    // Dependências
+    minhaFuncaoControler.$inject = ['$scope', 'meuService']; 
+      minhaFuncaoService.$inject = ['$http'];
 
-    meuControler.$inject = ['$scope', 'meuService']; //
-      meuService.$inject = ['$http'];
 
