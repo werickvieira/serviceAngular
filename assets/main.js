c12fbbf3c2b2d6b666b99bd8391d80608368e2a0
Werick <werick.vieira@yahoo.com.br>
2015-10-12 15:53:32 -0300

    Novos Ajustes

--- assets/main.js
+++ assets/main.js
@@ -1,36 +1,26 @@
 
-
 	var app = angular.module("exemploService",[]);
 	    app.controller("meuControler", meuControler );
 	    app.service("meuService", meuService);
 
 		    function meuControler ( $scope, meuService ){
 			  function sucesso ( obj ){
-		         $scope.User = obj.data;
+		        $scope.User = obj.data;
 			  }
 			  function erro ( err ){
-			     console.log( err );
-
+			    console.log( err );
 			  }; 
 
-			  meuService.getUser("werickvieira")
-			  .then(sucesso, erro);
-
-	    }; // Controller 
+			  meuService.then(sucesso, erro);
+			  
+	        }; // Controller 
 
 	    function meuService( $http ) {
-           
 	         var Requisicao = {
-		        url:'https://api.github.com/users/',
+		        url:'https://api.github.com/users/werickvieira',
 		        method:'GET'
 		      };
-
-		     this.getUser = function(login) {
-			  Requisicao.url += login;
-			   return $http(Requisicao);
-			 };
-	 
-
+			 return $http(Requisicao);
 	    }; // Service 
 
 
