33c5f07bb8afc49c902f4f9568fed9164ea69560
Werick <werick.vieira@yahoo.com.br>
2015-10-12 14:59:44 -0300

    Primeiro commit

---
+++ assets/main.js
@@ -0,0 +1,25 @@
+
+var app = angular.module("exemploService",[]);
+    app.controller("meuControler", meuControler );
+
+    function meuControler ( $scope, $http ){
+
+      var Requisicao = {
+        url:'https://api.github.com/users/werickvieira',
+        method:'GET'
+      };
+
+	  $http(Requisicao)
+	  .success(function( data ){
+        $scope.User = data;
+	  })
+	  .error(function( err ){
+	    console.log( err );
+
+	  }); 
+
+    };
+
+
+    meuControler.$inject = ['$scope', '$http']; //
+
