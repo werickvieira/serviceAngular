c0453bc620f5837371f4d52727bc90fd933998f7
Werick <werick.vieira@yahoo.com.br>
2015-10-12 15:30:17 -0300

    Alguns ajustes

--- assets/main.js
+++ assets/main.js
@@ -1,25 +1,39 @@
 
+
 	var app = angular.module("exemploService",[]);
 	    app.controller("meuControler", meuControler );
+	    app.service("meuService", meuService);
 
-	    function meuControler ( $scope, $http ){
+		    function meuControler ( $scope, meuService ){
+			  function sucesso ( obj ){
+		         $scope.User = obj.data;
+			  }
+			  function erro ( err ){
+			     console.log( err );
 
-	      var Requisicao = {
-	        url:'https://api.github.com/users/werickvieira',
-	        method:'GET'
-	      };
+			  }; 
 
-		  $http(Requisicao)
-		  .success(function( data ){
-	         $scope.User = data;
-		  })
-		  .error(function( err ){
-		     console.log( err );
+			  meuService.getUser("werickvieira")
+			  .then(sucesso, erro);
 
-		  }); 
+	    }; // Controller 
 
-	    };
+	    function meuService( $http ) {
+           
+	         var Requisicao = {
+		        url:'https://api.github.com/users/',
+		        method:'GET'
+		      };
+
+		     this.getUser = function(login) {
+			  Requisicao.url += login;
+			   return $http(Requisicao);
+			 };
+	 
+
+	    }; // Service 
 
 
-    meuControler.$inject = ['$scope', '$http']; //
+    meuControler.$inject = ['$scope', 'meuService']; //
+      meuService.$inject = ['$http'];
 
